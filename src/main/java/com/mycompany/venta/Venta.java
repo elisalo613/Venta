ab9cb8135623dfd51f50d002255259c47dd8528c
“elisalo613” <elisalo613@gmail.com>
2022-12-20 18:08:15 -0300

    First Commit

--- src/main/java/com/mycompany/venta/Venta.java
+++ src/main/java/com/mycompany/venta/Venta.java
@@ -13,5 +13,8 @@ public class Venta {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
