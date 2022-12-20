df2bcbb3f3847fb15ef14b3ca1f71b695c36679f
“elisalo613” <elisalo613@gmail.com>
2022-12-20 17:58:42 -0300

    First Commit

---
+++ src/main/java/com/mycompany/venta/Venta.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.venta;
+
+/**
+ *
+ * @author elisalo613
+ */
+public class Venta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
