c48272fc10e75f5268a872212dfc8a9a20cd29e2
“elisalo613” <elisalo613@gmail.com>
2022-12-29 21:05:41 -0300

    update 12-29

--- src/main/java/com/mycompany/venta/Venta.java
+++ src/main/java/com/mycompany/venta/Venta.java
@@ -4,17 +4,54 @@
  */
 
 package com.mycompany.venta;
+import java.util.*;
 
 /**
  *
  * @author elisalo613
  */
 public class Venta {
+    public static void main(String[] args){
+    Date Fecha;
+    Cliente cliente;
+    Vendedor vendedor;
+    List ListaDetalles;
+    int IDventa;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-    }
-}
+//    public static void main(String[] args) {
+//        Venta venta1 = new Venta();
+//        venta1.Fecha = 13Junio;
+//        System.out.println(venta1.Fecha);
+//        System.out.println(venta1.Cliente);
+//        System.out.println(venta1.Vendedor);
+//        System.out.println(venta1.ListaDetalles);
+//        System.out.println(venta1.IDventa);
+//    }
+}}
+
+class Vendedor {
+    String Nombre;
+    int Telefono;
+    String Mail;
+    List ListaVentas;
+    int IDvendedor;}
+
+class Detalle {
+    int Precio;
+    Producto producto;
+    int Cantidad;
+    int IDdetalle;}
+
+class Cliente {
+    String Nombre;
+    String Domicilio;
+    int Telefono;
+    List ListaVentas;
+    int IDcliente;}
+
+class Producto {
+    String Nombre;
+    Date Vencimiento;
+    int Precio;
+    int IDproducto;}
+
